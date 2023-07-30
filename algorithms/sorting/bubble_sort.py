b46ebb189829ed9715e057e81ac57de45b56d8b1
Alexey Zaets <alexey.zaets@jijiafrica.com>
2023-07-30 13:45:41 +0300

    refactored

--- algorithms/sorting/bubble_sort.py
+++ algorithms/sorting/bubble_sort.py
@@ -1,6 +1,8 @@
-def bubble_sort(items: list) -> None:
+def bubble_sort(items: list) -> list:
     # best case O(n)
     # average and worst case O(n**2)
-    for i in range(len(items) - 1):
-        if items[i] > items[i + 1]:
-            items[i], items[i + 1] = items[i + 1], items[i]
+    for _ in range(len(items) - 1, 0, -1):
+        for i in range(len(items) - 1):
+            if items[i] > items[i + 1]:
+                items[i], items[i + 1] = items[i + 1], items[i]
+    return items
